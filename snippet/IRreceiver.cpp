2ac8b818058b51d50904ad80ef7516c50140ce6c
marcoesposito1996 <marcoespositoh@gmail.com>
2021-04-16 19:23:37 +0200

    Add IRreceiver

---
+++ snippet/IRreceiver.cpp
@@ -0,0 +1,119 @@
+#include <Arduino.h>
+#include <IRremote.h> 
+
+#define V31
+
+#ifdef V27
+
+//VERSIONE 2.7
+IRrecv irrecv(13);
+decode_results res;
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("attivazione");
+  irrecv.enableIRIn();
+  Serial.println("attivato!");
+}
+
+void loop() {
+  if(irrecv.decode(&res)){
+    Serial.println(res.value, HEX);
+    irrecv.resume();
+  }
+  delay(100);
+}
+
+#endif
+
+#ifdef V31
+
+//VERSIONE 3.1 (più recente)
+
+
+/*
+ * Specify which protocol(s) should be used for decoding.
+ * If no protocol is defined, all protocols are active.
+ */
+//#define DECODE_DENON        // Includes Sharp
+//#define DECODE_JVC
+//#define DECODE_KASEIKYO
+//#define DECODE_PANASONIC    // the same as DECODE_KASEIKYO
+//#define DECODE_LG
+//#define DECODE_NEC          // Includes Apple and Onkyo
+//#define DECODE_SAMSUNG
+//#define DECODE_SONY
+//#define DECODE_RC5
+//#define DECODE_RC6
+
+//#define DECODE_BOSEWAVE
+//#define DECODE_LEGO_PF
+//#define DECODE_MAGIQUEST
+//#define DECODE_WHYNTER
+
+#define DECODE_HASH         // special decoder for all protocols
+
+
+
+/*
+ * Define macros for input and output pin etc.
+ */
+
+#define IR_RECEIVE_PIN          15  // D15
+#define IR_SEND_PIN              4  // D4
+#define tone(a,b,c) void()      // no tone() available on ESP32
+#define noTone(a) void()
+#define TONE_PIN                42 // Dummy for examples using it
+#define APPLICATION_PIN         16 // RX2 pin
+
+
+void setup() {
+    Serial.begin(115200);
+    // Just to know which program is running on my Arduino
+    Serial.println(F("START " __FILE__ " from " __DATE__ "\r\nUsing library version " VERSION_IRREMOTE));
+
+    /*
+     * Start the receiver, enable feedback LED and take LED feedback pin from the internal boards definition
+     */
+    IrReceiver.begin(IR_RECEIVE_PIN, ENABLE_LED_FEEDBACK, USE_DEFAULT_FEEDBACK_LED_PIN);
+
+    Serial.print(F("Ready to receive IR signals at pin "));
+    Serial.println(IR_RECEIVE_PIN);
+}
+
+void loop() {
+    /*
+     * Check if received data is available and if yes, try to decode it.
+     * Decoded result is in the IrReceiver.decodedIRData structure.
+     *
+     * E.g. command is in IrReceiver.decodedIRData.command
+     * address is in command is in IrReceiver.decodedIRData.address
+     * and up to 32 bit raw data in IrReceiver.decodedIRData.decodedRawData
+     */
+    if (IrReceiver.decode()) {
+
+        // Print a short summary of received data
+        IrReceiver.printIRResultShort(&Serial);
+        if (IrReceiver.decodedIRData.protocol == UNKNOWN) {
+            // We have an unknown protocol here, print more info
+            IrReceiver.printIRResultRawFormatted(&Serial, true);
+        }
+        Serial.println();
+
+        /*
+         * !!!Important!!! Enable receiving of the next value,
+         * since receiving has stopped after the end of the current received data packet.
+         */
+        IrReceiver.resume(); // Enable receiving of the next value
+
+        /*
+         * Finally, check the received data and perform actions according to the received command
+         */
+        if (IrReceiver.decodedIRData.command == 0x10) {
+            // do something
+        } else if (IrReceiver.decodedIRData.command == 0x11) {
+            // do something else
+        }
+    }
+}
+#endif
